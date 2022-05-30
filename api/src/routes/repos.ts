237769d32e4a7860edaab8e31039ff34cc464976
Bernadette <bern.roseabalos@gmail.com>
2022-05-30 17:34:35 -0400

    Add axios to handle request to github api

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,5 +1,18 @@
+/* eslint-disable no-console */
 import { Router, Request, Response } from 'express';
 import data from '../../data/repos.json';
+import axios from 'axios';
+
+const getData = () => {
+  return axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export const repos = Router();
 
@@ -7,7 +20,10 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
-
+  const results = await getData().then((resp) => {
+    return resp;
+  });
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([data]);
+
+  res.json(results);
 });
