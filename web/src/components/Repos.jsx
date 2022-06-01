266f33823fc3ea216d8c55f4456d301f203e7f46
Bernadette <bern.roseabalos@gmail.com>
2022-06-01 04:02:15 -0400

    Filter languages upon fetch and add to set

--- web/src/components/Repos.jsx
+++ web/src/components/Repos.jsx
@@ -2,44 +2,47 @@
 import { useEffect, useState } from 'react';
 
 const Repos = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+  const [languages, setLanguages] = useState([]);
+  const [selected, setSelected] = useState();
   useEffect(() => {
     fetch(`http://localhost:4000/repos`)
       .then((res) => res.json())
       .then((json) => {
-        setData(json);
+        setData(
+          json.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+        );
+        const filteredLanguages = new Set();
+        for (const repo of json) {
+          filteredLanguages.add(repo.language);
+        }
+        setLanguages(Array.from(filteredLanguages.values()));
       })
       .catch((err) => {
         console.log(err);
       });
-    return <h1>Repo List</h1>;
   }, []);
+
+  console.log('ayo', languages);
 
   return (
     <div>
       <h1>Fetched Data</h1>
+
       <ul>
-        {data &&
-          data
-            .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
-            .map(
-              ({
-                id,
-                name,
-                description,
-                language,
-                forks_count,
-                created_at,
-              }) => (
-                <li key={id}>
-                  <h3>Name: {name}</h3>
-                  <h3>Description: {description}</h3>
-                  <h3>Language: {language}</h3>
-                  <h3>Forks count: {forks_count}</h3>
-                  <h4>Date: {created_at}</h4>
-                </li>
-              )
-            )}
+        {data
+          .filter((repo) => (selected ? repo.language === selected : true))
+          .map(
+            ({ id, name, description, language, forks_count, created_at }) => (
+              <li key={id}>
+                <h3>Name: {name}</h3>
+                <h3>Description: {description}</h3>
+                <h3>Language: {language}</h3>
+                <h3>Forks count: {forks_count}</h3>
+                <h4>Date: {created_at}</h4>
+              </li>
+            )
+          )}
       </ul>
     </div>
   );
